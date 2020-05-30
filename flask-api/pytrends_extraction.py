5f0aee7199a063db94ab943c5f15d4e596916313
Tomas Angelini <tangelin@usc.edu>
2020-05-30 19:28:50 -0300

    small edit

--- flask-api/pytrends_extraction.py
+++ flask-api/pytrends_extraction.py
@@ -1,10 +1,5 @@
 from pytrends.request import TrendReq
-pytrends = TrendReq(hl='en-US', tz=360,
-                    timeout=(10,25),
-                    proxies=['https://34.203.233.13:80',],
-                    retries=2,
-                    backoff_factor=0.1,
-                    requests_args={'verify':False})
+pytrends = TrendReq(hl='en-US', tz=360)
 from datetime import datetime
 
 
